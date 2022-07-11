f8b16eaf87751c82a742110899d8c45124da7029
Satyendra230903 <108687896+Satyendra230903@users.noreply.github.com>
2022-07-11 11:30:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,109 +1,36 @@
-import streamlit as st 
-import numpy as np 
-
-import matplotlib.pyplot as plt
-from sklearn import datasets
+import pandas as pd
+import numpy as np
+  
+df = pd.read_csv('Iris.csv')
+df.head()
+# Dropping the Id column
+df.drop('Id', axis = 1, inplace = True)
+  
+# Renaming the target column into numbers to aid training of the model
+df['Species']= df['Species'].map({'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2})
+  
+# splitting the data into the columns which need to be trained(X) and the target column(y)
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+  
+# splitting data into training and testing data with 30 % of data as testing data respectively
 from sklearn.model_selection import train_test_split
-
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+  
+# importing the random forest classifier model and training it on the dataset
 from sklearn.ensemble import RandomForestClassifier
-
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+  
+# predicting on the test dataset
+y_pred = classifier.predict(X_test)
+  
+# finding out the accuracy
 from sklearn.metrics import accuracy_score
+score = accuracy_score(y_test, y_pred)
+# pickling the model
 
-st.title('Streamlit Example')
-
-st.write("""
-# Explore different classifier and datasets
-Which one is the best?
-""")
-
-dataset_name = st.sidebar.selectbox(
-    'Select Dataset',
-    ('Iris', 'Breast Cancer', 'Wine')
-)
-
-st.write(f"## {dataset_name} Dataset")
-
-classifier_name = st.sidebar.selectbox(
-    'Select classifier',
-    ('KNN', 'SVM', 'Random Forest')
-)
-
-def get_dataset(name):
-    data = None
-    if name == 'Iris':
-        data = datasets.load_iris()
-    elif name == 'Wine':
-        data = datasets.load_wine()
-    else:
-        data = datasets.load_breast_cancer()
-    X = data.data
-    y = data.target
-    return X, y
-
-X, y = get_dataset(dataset_name)
-st.write('Shape of dataset:', X.shape)
-st.write('number of classes:', len(np.unique(y)))
-
-def add_parameter_ui(clf_name):
-    params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0)
-        params['C'] = C
-    elif clf_name == 'KNN':
-        K = st.sidebar.slider('K', 1, 15)
-        params['K'] = K
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        params['n_estimators'] = n_estimators
-    return params
-
-params = add_parameter_ui(classifier_name)
-
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'SVM':
-        clf = SVC(C=params['C'])
-    elif clf_name == 'KNN':
-        clf = KNeighborsClassifier(n_neighbors=params['K'])
-    else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=1234)
-    return clf
-
-clf = get_classifier(classifier_name, params)
-#### CLASSIFICATION ####
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-
-acc = accuracy_score(y_test, y_pred)
-
-st.write(f'Classifier = {classifier_name}')
-st.write(f'Accuracy =', acc)
-
-#### PLOT DATASET ####
-# Project the data onto the 2 primary principal components
-pca = PCA(2)
-X_projected = pca.fit_transform(X)
-
-x1 = X_projected[:, 0]
-x2 = X_projected[:, 1]
-
-fig = plt.figure()
-plt.scatter(x1, x2,
-        c=y, alpha=0.8,
-        cmap='viridis')
-
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.colorbar()
-
-#plt.show()
-st.pyplot(fig)
+import pickle
+pickle_out = open("classifier.pkl", "wb")
+pickle.dump(classifier, pickle_out)
+pickle_out.close()
